0939bd3a3b510e18f316454249fd2ef5af4adc11
Emilianalila <emilianalila@gmail.com>
2023-11-01 11:11:42 +0100

    dog-years

---
+++ 2_dog-years/index.js
@@ -0,0 +1,15 @@
+// I declared a variable and asign my age to the value
+let myAge = 10;
+//using the operator * for create a value for my new variable
+let earlyYears = 2 * 10.5;
+//calculation the rest of the years using operators 
+let laterYears = (myAge -2) * 4;
+ 
+//adding the total value to my variable that give me the age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears)
+// declared my name and using a method to return a string with all lowercase letters.
+let myName = 'EMILIANA'.toLowerCase();
+console.log(myName)
+//showing in console our variables
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
