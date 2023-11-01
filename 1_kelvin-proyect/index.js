a7fb2dff23d30680eff4553d067aee065d3406a5
Emilianalila <emilianalila@gmail.com>
2023-11-01 10:59:21 +0100

    kelvin-proyect

---
+++ 1_kelvin-proyect/index.js
@@ -0,0 +1,54 @@
+/* // I declared a variable with the name kelvin and assign a value of 293
+let kelvin = 256;
+// I declared another variable and assign a new value wich it is a subtracting operation between my first variable less 273
+let celsius = kelvin - 273;
+// new variable declares with let, because we are going to change the value of kelvin's variable all the time, and also we add a method to help us to round the number we get 
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+console.log(`the temperature is: ${fahrenheit} degrees Fahrenheit.`); */
+
+
+
+// Grades
+// 1. Create a variable called "testGrade" which is a random number between 0 and 100.
+// 2. Create a series of conditionals that returns which letter grade corresponds with the score. For reference:
+// 0 to 65 is an "F"
+// 65 to 70 is a "D"
+// 71 to 79 is a "C"
+// 80 to 89 is a "B"
+// 90 to 100 is an "A"
+
+/* const testGrade = Math.floor(Math.random() * 100);
+if (testGrade >= 0 && testGrade < 65){
+  console.log(`your grade is: ${testGrade}, you get an "F"`)
+} else if(testGrade >= 65 && testGrade <= 70){
+  console.log(`your grade is: ${testGrade}, you get an "D"`)
+}else if (testGrade >= 71 && testGrade <= 79){
+  console.log(`your grade is: ${testGrade}, you get an "C"`)
+}else if(testGrade >= 80 && testGrade <= 89){
+  console.log(`your grade is: ${testGrade}, you get an "B"`)
+}else if (testGrade >=90 && testGrade <= 100){
+  console.log(`your grade is: ${testGrade}, you get an "A"`)
+}
+ */
+
+
+
+//Military Greetings
+const name = 'Emiliana';
+const rank = 'Private';
+let age = 78 ;
+
+if (age < 18){
+  console.log('You are too young for the military');
+}else if (name === 'Rugen' && rank === 'Count'){
+  console.log('My name is Inigo Montoya, you killed my father, prepare to die.')
+}else if (rank ==='Captian'){
+  console.log(`Aye Aye Captian ${name}.`);
+}else if (rank ==='Private'&& age > 40){
+  console.log(`what have you been doing with your life ${name}?`)
+}else{
+  console.log(`Hello ${rank}, ${name}, I cannot wait to celebrate your ${age + 1}th birthday next year!`)
+} 
+
+
